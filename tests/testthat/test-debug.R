2b167d25a5ed567ad1ed24d114237781d7a55b5e
Pascal Crépey <crepey@gmail.com>
2024-08-01 02:23:18 +0200

    tidy up tests

--- tests/testthat/test-debug.R
+++ tests/testthat/test-debug.R
@@ -6,3 +6,27 @@ test_that("test debug message", {
   expect_snapshot(Quiz$new(raw, debug = TRUE))
 
 })
+
+
+# test_that("no question found", {
+#   text = "This is not a question"
+#   expect_error(GIFTParser(text))
+# })
+#
+# test_that("no question found in the string", {
+#   text = "Mahatma Gandhi's birthday is an Indian holiday on  {
+# ~15th
+# ~3rd
+# =2nd
+# } of October.
+#
+# Since {
+#   ~495 AD
+#   =1066 AD
+#   ~1215 AD
+#   ~ 43 AD
+# }
+# the town of Hastings England has been 'famous with visitors'."
+#   res = GIFTParser(text, debug = TRUE)
+# })
+
--- tests/testthat/test-id_question_type.R
+++ tests/testthat/test-id_question_type.R
@@ -97,25 +97,3 @@ test_that("all types of questions", {
 
 })
 
-
-# test_that("no question found", {
-#   text = "This is not a question"
-#   expect_error(GIFTParser(text))
-# })
-#
-# test_that("no question found in the string", {
-#   text = "Mahatma Gandhi's birthday is an Indian holiday on  {
-# ~15th
-# ~3rd
-# =2nd
-# } of October.
-#
-# Since {
-#   ~495 AD
-#   =1066 AD
-#   ~1215 AD
-#   ~ 43 AD
-# }
-# the town of Hastings England has been 'famous with visitors'."
-#   res = GIFTParser(text, debug = TRUE)
-# })
